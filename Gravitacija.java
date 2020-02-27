7d5878aeb57f78190b5616e9e58b2a5391dc6f0f
JZorko <jan@zorko.net>
2020-02-27 15:55:15 +0100

    Delete Main

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,8 +1,4 @@
 public class Gravitacija {
-    public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
-    }
-    
     public static void polnIzpis(double nadmorskaVisina)
     {
         System.out.println("Nadmorska višina: " + nadmorskaVisina + "Pospešek: " + pospesek(nadmorskaVisina));
