c12a4f256f2c31f2d665daf741952f815daa42d7
Ubuntu <ubuntu@ip-172-31-40-46.ec2.internal>
2020-02-27 14:47:07 +0000

    Izpis Nadmorske višine in pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,10 @@
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
-    }    
+    }
+    
+    public static void polnIzpis(double nadmorskaVisina)
+    {
+        System.out.println("Nadmorska višina: "+nadmorskaVisina+ "Pospešek: "+izracun(nadmorskaVisina));
+    }
 }
