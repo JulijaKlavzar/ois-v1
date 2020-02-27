be244af07217f2f49f5c2cbc0a3d1cc1cc73172f
JZorko <jan@zorko.net>
2020-02-27 15:35:08 +0100

    Dodan izracun

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,13 @@
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
-    }    
+    }
+
+    private static double pospesek(double visina) {
+        final double konstanta = 6.674 * Math.pow(10, -11);
+		final double masa = 5.972 * Math.pow(10, 24);
+		final double polmer = 6.371 * Math.pow(10, 6);
+		
+		return (konstanta * masa) / ((polmer + visina) * (polmer + visina)); 
+	}
 }
